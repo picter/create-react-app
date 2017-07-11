e29343fc3429ea35f3969627bf9d4f749fdf2314
Richard <hello@noxan.net>
2017-07-11 10:55:56 +0200

    add babel preset latest and stage-3 plus transform-class-properties plugin

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -209,7 +209,12 @@ module.exports = {
         options: {
           // @remove-on-eject-begin
           babelrc: false,
-          presets: [require.resolve('babel-preset-react-app')],
+          presets: [
+            require.resolve('babel-preset-latest'),
+            require.resolve('babel-preset-react'),
+            require.resolve('babel-preset-stage-3'),
+          ],
+          plugins: ['transform-class-properties'],
           // @remove-on-eject-end
           // This is a feature of `babel-loader` for webpack (not Babel itself).
           // It enables caching results in ./node_modules/.cache/babel-loader/
