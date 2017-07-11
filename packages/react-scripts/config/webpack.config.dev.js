5d90d8c7bcb14d3356e5414bbc7eac896b642667
Richard <hello@noxan.net>
2017-07-11 10:54:25 +0200

    configure babel parser for eslint

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -149,6 +149,7 @@ module.exports = {
                     },
                   },
                 },
+                parser: 'babel-eslint',
               },
               ignore: false,
               useEslintrc: false,
